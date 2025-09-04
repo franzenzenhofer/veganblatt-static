848cc3ca13bdacece81c35d09d5a3bfff3e75edc
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-09-04 15:57:34 +0200

    fix: remove Twitter card meta tags - no Twitter support

--- src/templates/PageTemplate.ts
+++ src/templates/PageTemplate.ts
@@ -44,8 +44,6 @@ export class PageTemplate {
   <meta property="og:image" content="https://veganblatt.com/og-image.png">
   <meta property="og:image:width" content="1200">
   <meta property="og:image:height" content="630">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:image" content="https://veganblatt.com/og-image.png">
 </head>`;
   }
 
