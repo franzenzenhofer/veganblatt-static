4ce37b5b9d6e5263b282bd16456d72237977de42
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-09-07 20:41:52 +0000

    Add favicon references to HTML template

--- src/templates/TemplateEngine.ts
+++ src/templates/TemplateEngine.ts
@@ -30,6 +30,9 @@ export class TemplateEngine {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${fullTitle}</title>${metaTags}
+  <link rel="icon" type="image/x-icon" href="https://www.veganblatt.com/favicon.ico">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   <link rel="stylesheet" href="${cssHref}">
 </head>
 <body>
